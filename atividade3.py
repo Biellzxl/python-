9429761ede712044986e2ae52da69e15db66d58a
biellzxl <biellsps2309@gmail.com>
2022-10-27 21:13:10 -0300

    atividade_tres

---
+++ atividade3.py
@@ -0,0 +1,13 @@
+# bom dia boa tarde boa noite
+
+turno = input("Em qual turno voce estuda ?(M para matutino, V para Vespertino e N para noturno): ")
+match turno:
+    case "m":
+        print("bom dia :D")
+
+    case "v":
+        print("boa tarde :D")
+
+    case "n":
+        print("boa noite :D")
+
